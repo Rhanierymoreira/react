dd01983f5dbecbafbecd029c3d5ba1c71cdabdf4
Rhanierymoreira <rhaniery.4142@aluno.pr.senac.br>
2025-08-15 14:41:25 -0300

    update css and js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     <button
+  className="botao"
+  onClick={() => window.location.href = "https://rhanierymoreira.github.io/portfolio/"}
+>
+  Algum botão
+</button>
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Rhaniery Moreira</code> and save to reload.
+          Edit <code>Rhaniery Moreira</code> and install a trojan in your computer free.
         </p>
         
         <a
           className="App-link"
-          href="https://rhanierymoreira.github.io/portfolio/"
+          href="https://vercel.com/rhanierymoreiras-projects"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ola Mundo! Entrem no meu Portfolioo
+          Ola Mundo! Entrem no meu vercel
         </a>
         <div class="space">Holá, que tal?</div>
+        
       </header>
     </div>
   );
